04b1088a0091afc9833e504e1670dcb9abe8e5a3
@talhajamal89 <155877254+talhajamal1@users.noreply.github.com>
2024-02-25 22:30:24 +0500

    First Login-Signup-UI Project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'landing-login-page.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login Signup UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => Login(),
+      },
+    );
+  }
+}
